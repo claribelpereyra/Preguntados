eadc13831bc07b7ca054ce9cb43de81605114f41
Daniela Melian <meliandaani@gmail.com>
2024-05-28 14:17:08 -0300

    Se armo la estructura basica del juego en modo singleplayer: sistema de vidas, eleccion de categorias, sistema de puntuacion, y posibles salidas: ganar, abandonar y perder

--- main.py
+++ main.py
@@ -3,94 +3,114 @@ import random
 ######### CONSTANTES ##################
 tematicas = ["HISTORIA", "DEPORTE", "CIENCIA", "ARTE", "GEOGRAFIA", "ENTRETENIMIENTO"]
 
-preg_historia = [[1, "pregunta?", "opcion1", "opcion2", "opcion3"],
-                 [2, "pregunta?", "opcion1", "opcion2", "opcion3"]]
-preg_deporte = [[1, "pregunta?", "opcion1", "opcion2", "opcion3"],
-                [2, "pregunta?", "opcion1", "opcion2", "opcion3"]]
-preg_ciencia = [[1, "pregunta?", "opcion1", "opcion2", "opcion3"],
-                [2, "pregunta?", "opcion1", "opcion2", "opcion3"]]
-preg_arte = [[1, "pregunta?", "opcion1", "opcion2", "opcion3"],
-             [2, "pregunta?", "opcion1", "opcion2", "opcion3"]]
-preg_geografia = [[1, "pregunta?", "opcion1", "opcion2", "opcion3"],
-                  [2, "pregunta?", "opcion1", "opcion2", "opcion3"]]
-preg_entretenimiento = [[1, "pregunta?", "opcion1", "opcion2", "opcion3"],
-                        [2, "pregunta?", "opcion1", "opcion2", "opcion3"]]
-
 
 ###########  FUNCIONES ###########
 
 def eleccion_tematica():
     # TODO: Validaciones de input
+    print("Bienvenido al preguntados, estas son las categorias disponibles: ")
+    for i in range(len(tematicas)):
+        print(i + 1, " - ", tematicas[i])
+    print("O ingrese FIN para salir del juego. ")
     # Aca tambien puede elegir si terminar el juego o no
-    tematica = int(input("Ingrese la tematica ...... : "))
+    opcion_elegida = input("Ingrese la opcion elegida: ")
 
+    if opcion_elegida != "FIN":
+        tematica = tematicas[int(opcion_elegida) - 1]
+        del tematicas[int(opcion_elegida) - 1]
+    else:
+        tematica = opcion_elegida
     return tematica
 
 
 def eleccion_preguntas(tematica):
     nro_preg = random.randint(0, 1)
 
-    if tematica == 1:
+    if tematica == 'HISTORIA':
         if nro_preg == 0:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta historia 1?", "opcion1", "opcion2", "opcion3", 1]
         elif nro_preg == 1:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta historia 2?", "opcion1", "opcion2", "opcion3", 1]
 
-    elif tematica == 2:
+    elif tematica == "DEPORTE":
         if nro_preg == 0:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta deporte 1?", "opcion1", "opcion2", "opcion3", 1]
         elif nro_preg == 1:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta deporte 2?", "opcion1", "opcion2", "opcion3", 1]
 
-    elif tematica == 3:
+    elif tematica == "CIENCIA":
         if nro_preg == 0:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta ciencia 1?", "opcion1", "opcion2", "opcion3", 1]
         elif nro_preg == 1:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta ciencia 2?", "opcion1", "opcion2", "opcion3", 1]
 
-    elif tematica == 4:
+    elif tematica == "ARTE":
         if nro_preg == 0:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta arte 1?", "opcion1", "opcion2", "opcion3", 1]
         elif nro_preg == 1:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta arte 2?", "opcion1", "opcion2", "opcion3", 1]
 
-    elif tematica == 5:
+    elif tematica == "GEOGRAFIA":
         if nro_preg == 0:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta geografia 1?", "opcion1", "opcion2", "opcion3", 1]
         elif nro_preg == 1:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta geografia 2 ?", "opcion1", "opcion2", "opcion3", 1]
 
     else:
         if nro_preg == 0:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta entretenimiento 1 ?", "opcion1", "opcion2", "opcion3", 1]
         elif nro_preg == 1:
-            pregunta = ["pregunta?", "opcion1", "opcion2", "opcion3", "opcionok"]
+            pregunta = ["pregunta? entretenimiento 2 ?", "opcion1", "opcion2", "opcion3", 1]
 
     return pregunta
 
 
 def ejecucion_juego():
     puntos = 0
-    tematica = 0
-    
-    while tematica != -1:
-    
-        tematica = eleccion_tematica()
+    vidas = 5
+    tematica = eleccion_tematica()
+
+    while tematica != "FIN" and vidas > 0:
         pregunta = eleccion_preguntas(tematica)
-        
-        for i in range(len(pregunta-1)):
+
+        for i in range(len(pregunta) - 1):
             print(pregunta[i])
-        
+
         opcion_elegida = int(input("Ingrese su respuesta: "))
-        
+
         if opcion_elegida == pregunta[4]:
             print("Respuesta correcta")
             puntos += 10
-        else: 
+        else:
             print("Respuesta incorrecta")
-        
-        tematica = eleccion_tematica()
+            while (opcion_elegida != pregunta[4]) and vidas > 0:
+                vidas -= 1
+                print("Te quedan ", vidas, "vidas.")
 
-print("Respuestas correctas y puntos")
-    
-    
+                if vidas > 0:
+                    pregunta = eleccion_preguntas(tematica)
+
+                    for i in range(len(pregunta) - 1):
+                        print(pregunta[i])
+
+                    opcion_elegida = int(input("Ingrese su respuesta: "))
+
+                    if opcion_elegida == pregunta[4]:
+                        print("Respuesta correcta")
+                        puntos += 5
+        if len(tematicas) > 0 and vidas > 0:
+            tematica = eleccion_tematica()
+        else:
+            tematica = "FIN"
+
+    if vidas > 0 and len(tematicas) == 0:
+        print("GANASTE!!!")
+        print("Respuestas correctas y puntos, ", puntos)
+    elif vidas > 0 and len(tematicas) > 0:
+        print("ABANDONASTE")
+
+    elif vidas == 0 and len(tematicas) > 0:
+        print("PERDISTE.")
+
+
+ejecucion_juego()
